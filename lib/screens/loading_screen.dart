53638b6893eb943d36d93a203207c907dbc62642
Ryuken07 <itsmevasu84@gmail.com>
2021-03-14 16:25:43 +0530

    Adding files to git

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,20 +1,46 @@
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
+import 'package:clima/services/location.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+const apiKey = 'c022ae64068e9eac36125fa4c4011b33';
 
 class LoadingScreen extends StatefulWidget {
   @override
-  _LoadingScreenState createState() => _LoadingScreenState();
+  State<StatefulWidget> createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        'http://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
+    }));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
+        child: SpinKitThreeBounce(
+          color: Colors.white,
+          size: 40.0,
         ),
       ),
     );
